271d611eb41a2ced6c92d9ede218be799d95efe8
ylliu <liuming1908@gmail.com>
2022-07-18 17:02:31 +0800

    add Point

---
+++ src/main/java/Point.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point move(Direction direction) {
+        switch (direction) {
+            case EAST -> this.x++;
+            case NORTH -> this.y++;
+            case WEST -> this.x--;
+            case SOUTH -> this.y--;
+        }
+        return new Point(this.x, this.y);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
